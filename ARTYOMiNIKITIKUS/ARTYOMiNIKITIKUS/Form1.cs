3a2a270bede9a50633e311898bb66549fdf2d43f
IHateBeingAlone <artemdedovec-99@mail.ru>
2018-07-03 11:36:20 +0600

    Create SimpleOpperation

--- ARTYOMiNIKITIKUS/ARTYOMiNIKITIKUS/Form1.cs
+++ ARTYOMiNIKITIKUS/ARTYOMiNIKITIKUS/Form1.cs
@@ -15,5 +15,65 @@ namespace ARTYOMiNIKITIKUS
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string FirstValueText = textBox1.Text;
+            double FirstValue = Convert.ToDouble(FirstValueText);
+            string SecondValueText = textBox2.Text;
+            double SecondValue = Convert.ToDouble(SecondValueText);
+            double result = FirstValue - SecondValue;
+            label1.Text = result.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string FirstValueText = textBox1.Text;
+            double FirstValue = Convert.ToDouble(FirstValueText);
+            string SecondValueText = textBox2.Text;
+            double SecondValue = Convert.ToDouble(SecondValueText);
+            double result = FirstValue + SecondValue;
+            label1.Text = result.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string FirstValueText = textBox1.Text;
+            double FirstValue = Convert.ToDouble(FirstValueText);
+            string SecondValueText = textBox2.Text;
+            double SecondValue = Convert.ToDouble(SecondValueText);
+            double result = FirstValue * SecondValue;
+            label1.Text = result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string FirstValueText = textBox1.Text;
+            double FirstValue = Convert.ToDouble(FirstValueText);
+            string SecondValueText = textBox2.Text;
+            double SecondValue = Convert.ToDouble(SecondValueText);
+            double result = FirstValue / SecondValue;
+            label1.Text = result.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
