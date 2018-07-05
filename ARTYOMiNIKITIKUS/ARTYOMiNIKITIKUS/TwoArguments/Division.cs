1f56cacad3cd988777bc565b450bc65083a70a02
IHateBeingAlone <artemdedovec-99@mail.ru>
2018-07-05 11:02:09 +0600

    Add DivisionByZero

--- ARTYOMiNIKITIKUS/ARTYOMiNIKITIKUS/TwoArguments/Division.cs
+++ ARTYOMiNIKITIKUS/ARTYOMiNIKITIKUS/TwoArguments/Division.cs
@@ -1,9 +1,15 @@
-﻿namespace ARTYOMiNIKITIKUS
+﻿using System;
+
+namespace ARTYOMiNIKITIKUS
 {
     public class Division: ITwoArgumentsCalculator
     {
         public double Calculate(double firstArgument, double secondArgument)
         {
+            if (secondArgument == 0)
+            {
+                throw new Exception("OSHIPKA! NE DELI NA NOL', DOLBOEB");
+            }
             return firstArgument / secondArgument;
         }
     }
